665fe45381e22789e66b4cfcb06b49c34b2e436b
Dominik Kawczak <dominik.kawczak@iteo.com>
2022-09-04 22:20:52 +0200

    Finish InitOnly Homework

--- Module01-RecordsPart1/src/InitOnly/CarFactoryTests.cs
+++ Module01-RecordsPart1/src/InitOnly/CarFactoryTests.cs
@@ -24,23 +24,26 @@ namespace InitOnly
 
     class CarFactory
     {
-        public static ICar CreateCar<TCar>(string name, int horsePower) where TCar : ICar
+        public static ICar CreateCar<TCar>(string name, int horsePower) where TCar : ICar, new()
         {
-            var car = new TCar();
-            car.Name = name;
-            car.HorsePower = horsePower;
-            return car;
+            return new TCar
+            {
+                Name = name,
+                HorsePower = horsePower
+            };
         }
     }
 
-    class Toyota
+    class Toyota : ICar
     {
+        public string Name { get; init; }
+        public int HorsePower { get; init; }
     }
 
     interface ICar
     {
-        string Name { get; set; }
+        string Name { get; init; }
 
-        int HorsePower { get; set; }
+        int HorsePower { get; init; }
     }
 }
